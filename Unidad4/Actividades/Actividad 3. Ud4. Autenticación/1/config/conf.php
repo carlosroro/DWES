cc7966795b497886e8f63a0bcc777751ae7c22e8
carlosroro <168224408+carlosroro@users.noreply.github.com>
2025-12-14 21:20:40 +0100

    Add files via upload

---
+++ Unidad4/Actividades/Actividad 3. Ud4. Autenticación/1/config/conf.php
@@ -0,0 +1,16 @@
+<?php
+// Array con los usuarios
+$users = [
+    ["usuario", "1234"],
+    ["admin", "12345"],
+    ["alumno", "hola1234"]
+];
+
+// Función para limpiar datos
+function clearData($cadena) {
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+    $cadena = htmlspecialchars($cadena);
+    return $cadena;
+}
+?>
